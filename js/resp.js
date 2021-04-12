8f6f5eef2545880e7f4de96035e328fec341dd3f
sribanky <41723381+sribanky@users.noreply.github.com>
2021-04-12 19:53:52 +0530

    Add files via upload

---
+++ js/resp.js
@@ -0,0 +1,10 @@
+burger = document.querySelector('.burger')
+rightnav = document.querySelector('.rightnav')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+
+burger.addEventListener ('click' , () => {
+// rightnav.classList.toggle('visibility');
+// nav-list.classList.toggle('visibility');
+navbar.classList.toggle('heightNav');
+})
